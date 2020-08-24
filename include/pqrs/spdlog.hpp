af27d5d35d1ff5246202832595b2690509803983
Takayama Fumihiko <tekezo@pqrs.org>
2020-08-24 23:45:47 +0900

    Add pqrs::spdlog::factory

--- include/pqrs/spdlog.hpp
+++ include/pqrs/spdlog.hpp
@@ -6,6 +6,7 @@
 // Distributed under the Boost Software License, Version 1.0.
 // (See http://www.boost.org/LICENSE_1_0.txt)
 
+#include "spdlog/factory.hpp"
 #include "spdlog/monitor.hpp"
 #include "spdlog/reader.hpp"
 #include "spdlog/spdlog.hpp"
